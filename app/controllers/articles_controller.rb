eab0033db8d2cb46377d8895e4bee15ab397ef23
koh-suzuki <k.s-coward-0629@ezweb.ne.jp>
2020-04-19 16:18:59 +0000

    blogpage 作成中 show

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,49 @@
+class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :destroy, :update]
+  def index
+    @articles = Article.order(created_at: :desc)
+  end
+  
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    @article = Article.new(params_article)
+    if @article.save
+      redirect_to articles_path, notice: "新規作成できました。"
+    else
+      render :new, alert: "作成に失敗しました。"
+    end
+  end
+
+  def edit
+  end
+  
+  def update
+    if @article.update(params_article)
+      redirect_to articles_path, notice: "更新できました。"
+    else
+      render :edit, alert: "更新に失敗しました。"
+    end
+  end
+
+  def destroy
+    if @article.destroy
+      redirect_to articles_path, notice: "削除しました。"
+    else
+      render :show, alert: "削除できませんでした。"
+    end
+  end
+  
+  private
+    def find_article
+      @article = Article.find(params[:id])
+    end
+    
+    def params_article
+    end
+end
